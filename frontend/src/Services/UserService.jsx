bd75ccc82053f73dab744d3f77897d985a02c7d2
Rutik <rutikgawali100@gmail.com>
2025-12-26 19:34:26 +0530

    Sending OTP to mail for Reset Password Feature completed

--- frontend/src/Services/UserService.jsx
+++ frontend/src/Services/UserService.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-const base_url = "http://localhost:8080/users/"
+const base_url = "http://localhost:9090/users/"
 const registerUser = async (user)=>{
     return axios.post(`${base_url}register`, user)
     .then(res=> res.data)
@@ -12,4 +12,22 @@ const loginUser = async (login)=>{
     .catch(error=> {throw error;});
     
 }
-export {registerUser, loginUser};
+const sendOtp = async (email) => {
+    return axios.post(`${base_url}sendOTP/${email}`)
+    .then(result => result.data)
+    .catch(error => {throw error;});
+}
+
+const verifyOtp= async (email, otp)=> {
+    return axios.get(`${base_url}verifyOTP/${email}/${otp}`)
+    .then(result => result.data)
+    .catch(error => {throw error;});
+}
+
+const changePassword = async(email, password) => {
+    return axios.post(`${base_url}changePassword`, {email, password})
+    .then(result => result.data)
+    .catch(error => {throw error;});
+}
+
+export {registerUser, loginUser, sendOtp, verifyOtp, changePassword};
